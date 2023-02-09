30e906f3690ba8cc00c31373b83216e36007feab
Your Name <107371667+mohammedalsersawi@users.noreply.github.com>
2023-02-09 16:10:27 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\ModelController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Car\ModelController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
