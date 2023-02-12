763c6df7d8fa2b7024b96baeed0f1bab9ec4424f
Your Name <107371667+mohammedalsersawi@users.noreply.github.com>
2023-02-13 00:27:11 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -31,14 +31,12 @@ Route::group(
         Route::prefix('admin')->middleware('auth')->group(function () {
             Route::view('index', 'admin.part.app');
         });
-
         Route::controller(ModelController::class)->group(function () {
             Route::get('/model', 'index')->name('model');
             Route::post('/model/store', 'store')->name('model.store');
             Route::GET('/model/edit/{id?}', 'edit')->name('model.edit');
             Route::delete('/model/delete/{id?}', 'destroy')->name('model.delete');
             Route::get('model/getData', 'getData')->name('model.getData');
-
         });
         Route::controller(EngineController::class)->group(function () {
             Route::get('/engines', 'index')->name('engines');
@@ -46,7 +44,6 @@ Route::group(
             Route::GET('/engines/edit/{id?}', 'edit')->name('engines.edit');
             Route::delete('/engines/delete/{id?}', 'destroy')->name('engines.delete');
             Route::get('engines/getData', 'getData')->name('engines.getData');
-
         });
         Route::controller(FuelTypeController::class)->group(function () {
             Route::get('/fuel/type', 'index')->name('fuel_type');
@@ -54,7 +51,6 @@ Route::group(
             Route::GET('/fuel/type/edit/{id?}', 'edit')->name('fuel_type.edit');
             Route::delete('/fuel/type/delete/{id?}', 'destroy')->name('fuel_type.delete');
             Route::GET('fuel/type/getData', 'getData')->name('fuel_type.getData');
-
         });
         Route::controller(BrandController::class)->group(function () {
             Route::get('/brand', 'index')->name('brand');
@@ -62,7 +58,6 @@ Route::group(
             Route::GET('/brand/edit/{id?}', 'edit')->name('brand.edit');
             Route::delete('/brand/delete/{id?}', 'destroy')->name('brand.delete');
             Route::get('brand/getData', 'getData')->name('brand.getData');
-
         });
         Route::resource('room',RoomController::class)->except(['create','update']);
         Route::resource('color',ColorController::class)->except(['create','show','update']);
