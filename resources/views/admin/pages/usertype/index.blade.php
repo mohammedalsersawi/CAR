1f52d65d6ba651b259ae1f745045875967eeaea7
Your Name <107371667+mohammedalsersawi@users.noreply.github.com>
2023-02-21 13:35:40 +0200

    fix

--- resources/views/admin/pages/usertype/index.blade.php
+++ resources/views/admin/pages/usertype/index.blade.php
@@ -277,6 +277,37 @@
                         </div>
 
 
+
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="">@lang('type')</label>
+                                <select name="user_type_id" id="" class="select form-control"
+                                    data-select2-id="select2-data-1-bgy2" tabindex="-1" aria-hidden="true">
+                                    <option selected disabled>Select @lang('type')</option>
+                                    @foreach ($user as $itemm)
+                                        <option value="{{ $itemm->id }}"> {{ $itemm->Name }} </option>
+                                        </option>
+                                    @endforeach
+                                </select>
+                                <div class="invalid-feedback"></div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="phone">@lang('password')</label>
+                                <input type="password" class="form-control" placeholder="@lang('password')"
+                                    name="password" id="phone">
+                                <div class="invalid-feedback"></div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="image">@lang('image')</label>
+                                <input type="file" accept="image/*" class="form-control"
+                                    placeholder="@lang('image')" name="image" id="image">
+                                <div class="invalid-feedback"></div>
+                            </div>
+                        </div>
                         <div id="map"></div>
                         <input type="hidden" name="lat" id="lat">
                         <input type="hidden" name="lng" id="lng">
@@ -399,6 +430,28 @@
 
 
 
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="">@lang('type')</label>
+                                <select name="user_type_id" id="edit_type" class="select form-control"
+                                    data-select2-id="select2-data-1-bgy2" tabindex="-1" aria-hidden="true">
+                                    <option selected disabled>Select @lang('type')</option>
+                                    @foreach ($user as $itemm)
+                                        <option value="{{ $itemm->id }}"> {{ $itemm->Name }} </option>
+                                        </option>
+                                    @endforeach
+                                </select>
+                                <div class="invalid-feedback"></div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="image">@lang('image')</label>
+                                <input type="file" accept="image/*" class="form-control"
+                                    placeholder="@lang('image')" name="image" id="image">
+                                <div class="invalid-feedback"></div>
+                            </div>
+                        </div>
                         <div id="map2"></div>
                         <input type="hidden" name="lat" id="edit_lat">
                         <input type="hidden" name="lng" id="edit_lng">
