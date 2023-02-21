c23017ae7295e9a1662d34a94fdf6d43849ae769
mohamed-salhi <96651239+mohamed-salhi@users.noreply.github.com>
2023-02-21 14:07:51 +0200

    fix salhi

---
+++ database/migrations/2023_02_21_101151_create_cars_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->primary();
+            $table->foreignId('brand_id')->constrained('brands')->cascadeOnUpdate();
+            $table->foreignId('model_id')->constrained('model_cars')->cascadeOnUpdate();
+            $table->foreignId('engine_id')->constrained('engines')->cascadeOnUpdate();
+            $table->foreignId('transmission_id')->constrained('transmissions')->cascadeOnUpdate();
+            $table->foreignId('fule_type_id')->constrained('fule_types')->cascadeOnUpdate();
+            $table->foreignId('color_exterior_id')->constrained('colors')->cascadeOnUpdate();
+            $table->foreignId('color_interior_id')->constrained('colors')->cascadeOnUpdate();
+            $table->integer('mileage');
+            $table->integer('year_from');
+            $table->integer('year_to');
+            $table->string('phone');
+            $table->double('lat');
+            $table->double('leg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
