f7c2c305a457c723b779d53d78326d200ac02db0
Your Name <107371667+mohammedalsersawi@users.noreply.github.com>
2023-02-26 13:10:14 +0200

    fix

--- app/Models/year.php
+++ app/Models/year.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class year extends Model
+class Year extends Model
 {
     use HasFactory;
-    protected $guarded=[];
+    protected $guarded = [];
+
 }
